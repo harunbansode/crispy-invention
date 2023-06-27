5194f7870a3301c4ff322327c166bd1dcd41297c
Harun Bansode <harun.bansode@gmail.com>
2023-06-27 22:27:50 +0530

    second commit

---
+++ Word_Replacement_Program/main.py
@@ -0,0 +1,7 @@
+def word_replacement():
+    str = "Hi, This is Harun Bansode. I'm a good Harun. "
+    word_to_replace = input('Enter a word: ')
+    replacement_word = input('Enter the replacement word: ')
+    print(str.replace(word_to_replace, replacement_word))
+
+word_replacement()
